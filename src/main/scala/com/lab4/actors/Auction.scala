c22ea92aa985c540d824e799dc1540fbebcb2969
PaulinaLach <smig007@wp.pl>
2016-11-17 22:21:02 +0100

    push

--- src/main/scala/com/lab4/actors/Auction.scala
+++ src/main/scala/com/lab4/actors/Auction.scala
@@ -1,8 +1,11 @@
 package com.lab4.actors
 
 import akka.actor.{ActorRef, FSM}
+import akka.persistence.fsm.PersistentFSM
 
 import scala.concurrent.duration._
+import scala.reflect.ClassTag
+import scala.reflect._
 
 case class Bid(amount: Float)
 case class WinAuction(amount: Float, i: String)
@@ -25,36 +28,60 @@ case object NotInitialized extends Data
 case class Initialized(title: String, seller: ActorRef) extends Data
 case class HasBeenBid(title: String, bidValue: Float, seller: ActorRef, winner: ActorRef) extends Data
 
-class Auction extends FSM[State, Data] {
+//////////
+sealed trait AuctionEvent
+sealed trait AuctionEventWithTime extends AuctionEvent {
+  val currentMillis = System.currentTimeMillis
+}
+
+case class AuctionCreatedEvent() extends AuctionEventWithTime
+case class AuctionIgnoredEvent() extends AuctionEventWithTime
+case class AuctionActivatedEvent() extends AuctionEventWithTime
+case class AuctionIgnoredEvent() extends AuctionEventWithTime
+//case class BuyerBidEvt(val price: BigDecimal) extends AuctionEventWithTime
+//case class BuyerJoinedEvt(val price: BigDecimal) extends AuctionEventWithTime
+///////////
+
+case class BuyerSubscribedEvt() extends AuctionEvent
+case object AuctionInitializedEvent extends AuctionEvent
+case object AuctionSoldEvt extends AuctionEvent
+case class AuctionClosedEvt() extends AuctionEventWithTime
+
+
+
+class Auction(val name: String) extends PersistentFSM[State, Data, AuctionEvent] {
+  override def persistenceId = "persistent-" + name
+  override def domainEventClassTag: ClassTag[AuctionEvent] = classTag[AuctionEvent]
+
   startWith(InitialState, NotInitialized)
 
   when(InitialState) {
     case Event(InitializeAuction(title), NotInitialized) =>
       context.actorSelection("/user/auctionSearch") ! SubscribeToSearch(title)
-      goto(Created) using Initialized(title, sender)
+      goto(Created) applying AuctionInitializedEvent // using Initialized(title, sender)
   }
 
   when(Created, stateTimeout = 10 seconds) {
     case Event(StateTimeout, initialized: Initialized) =>
       println(s"Auction ${self.toString()} -> ignored")
-      goto(Ignored) using initialized
+      goto(Ignored) applying AuctionInitializedEvent // using initialized
 
     case Event(Bid(amount), initialized: Initialized) =>
       var auctionData = HasBeenBid(initialized.title, 0.0f, initialized.seller, sender)
       auctionData = bidAuction(auctionData, amount)
-      goto(Activated) using auctionData
+      goto(Activated) applying AuctionActivatedEvent // using auctionData
     case Event(_, initialized: Initialized) =>
       sender ! FailedBid
-      stay using initialized
+      stay applying AuctionIgnoredEvent
   }
 
   when(Ignored, stateTimeout = 20 seconds) {
     case Event(Relist, initialized: Initialized) =>
       println(s"Auction ${self.toString()} -> relist")
-      goto(Created) using initialized
+      goto(Created) applying AuctionCreatedEvent
     case Event(DeleteTimer, initialized: Initialized) =>
       println(s"Auction ${self.toString()} -> delete")
-      stay using initialized
+      stay applying
   }
 
   when(Activated, stateTimeout = 10 seconds) {
